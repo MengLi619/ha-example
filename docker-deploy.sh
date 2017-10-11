428d25d9e4160fc4c566a5dd899b0237aadf10b4
Mengli <dream83619@gmail.com>
2017-10-11 09:02:04 +0800

    initial commit

--- docker-deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-scp docker-compose.yml root@47.95.220.253:/root
-#docker stack deploy -c docker-compose.yml ha-example
--- docker-push.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-docker tag mengli/ha-example/mysql-group-replication:latest registry.cn-beijing.aliyuncs.com/mengli/ha-example_mysql-group-replication:latest
-docker tag mengli/ha-example/redis-sentinel:latest registry.cn-beijing.aliyuncs.com/mengli/ha-example_redis-sentinel:latest
-docker tag mengli/ha-example/ha-example-server:latest registry.cn-beijing.aliyuncs.com/mengli/ha-example_ha-example-server:latest
-
-docker push registry.cn-beijing.aliyuncs.com/mengli/ha-example_mysql-group-replication:latest
-docker push registry.cn-beijing.aliyuncs.com/mengli/ha-example_redis-sentinel:latest
-docker push registry.cn-beijing.aliyuncs.com/mengli/ha-example_ha-example-server:latest
